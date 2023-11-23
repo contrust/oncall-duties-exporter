ad0ea18d008518ee83f5abf666b1dc1ac3843564
Artyom Borisov <2001raf@gmail.com>
2023-11-23 14:48:16 +0500

    Added a dot in the end of the description of the metric-update-time-in-seconds option.

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def parse_arguments():
         type=float,
         default=DEFAULT_METRICS_UPDATE_TIME_IN_SECONDS,
         help='the time in seconds after the app is trying to update metrics, '
-             f'{DEFAULT_METRICS_UPDATE_TIME_IN_SECONDS} by default'
+             f'{DEFAULT_METRICS_UPDATE_TIME_IN_SECONDS} by default.'
     )
 
     parser.add_argument(
